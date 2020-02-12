739bed3a03545dff91e24a1fc74417469d246748
Timotheus S <timotheus.schoene@gmx.de>
2020-02-12 15:55:44 +0100

    exclude _config.yml

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,9 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 function doIt() {
 	rsync -v --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
+		--exclude "_config.yml
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 	test -f ~/.bash_profile && source ~/.bash_profile ;
 }
