cef3dea1404d8771cd5dbcdc2154ae4d73a4c559
Timotheus Schöne <timotheus.schoene@gmx.de>
2020-02-12 15:43:52 +0100

    test -f ~/.bash_profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ function doIt() {
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	test -f ~/.bash_profile && source ~/.bash_profile ;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
